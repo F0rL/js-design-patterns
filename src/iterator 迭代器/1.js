82649b9bea8ddf614f9a2b8b5c92100831d36d53
F0rl <F0rL@users.noreply.github.com>
2019-11-01 20:52:51 +0800

    迭代器

--- src/iterator 迭代器/1.js
+++ src/iterator 迭代器/1.js
@@ -1,2 +1,39 @@
-// 顺序访问一个集合
+// 顺序访问一个有序集合
 // 使用者无需知道集合内部结构
+
+class Iterator {
+  constructor(container) {
+    this.list = container.list
+    this.index = 0
+  }
+  next(){
+    if(this.hasNext()){
+      return this.list[this.index++]
+    }
+    return null
+  }
+  hasNext(){
+    if(this.index >= this.list.length){
+      return false
+    }
+    return true
+  }
+}
+
+class Container {
+  constructor(list) {
+    this.list = list
+  }
+  // 生成遍历器
+  getInterator(){
+    return new Iterator(this)
+  }
+}
+
+const arr = [1,2,3,4,5,6,7,8]
+
+let container = new Container(arr)
+let iterator = container.getInterator()
+while(iterator.hasNext()){
+  console.log(iterator.next())
+}
